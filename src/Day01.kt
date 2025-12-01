782ee94a24de96d8c998e8b73b15f2874a161e5a
Viacheslav Romaniuk <net.highlander@gmail.com>
2025-12-01 14:01:24 +0100

    Day 01 complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,40 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var stoppingPoint = 50
+        var zeroCounter = 0
+        input.forEach { line ->
+            val direction = if (line.first() == 'L') -1 else 1
+            val distance = line.drop(1).toInt()
+            stoppingPoint += (direction * distance) % 100
+            if (stoppingPoint < 0) stoppingPoint = 100 + stoppingPoint % 100
+            if (stoppingPoint > 99) stoppingPoint %= 100
+            if (stoppingPoint == 0) zeroCounter++
+        }
+        return zeroCounter
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var stoppingPoint = 50
+        var zeroCounter = 0
+        input.forEach { line ->
+            val direction = if (line.first() == 'L') -1 else 1
+            val distance = line.drop(1).toInt()
+            val nextPosition = stoppingPoint + direction * distance
+            zeroCounter += if (nextPosition > stoppingPoint) {
+                Math.floorDiv(nextPosition, 100) - Math.floorDiv(stoppingPoint, 100)
+            } else {
+                Math.floorDiv(stoppingPoint - 1, 100) - Math.floorDiv(nextPosition - 1, 100)
+            }
+            stoppingPoint = nextPosition
+            stoppingPoint = Math.floorMod(stoppingPoint, 100)
+        }
+        return zeroCounter
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
+    // Read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
