abf3c43756de7ab2e2a4ebabc19a2e605c88500f
Viacheslav Romaniuk <net.highlander@gmail.com>
2025-12-15 10:56:54 +0100

    Day 11 partially complete

---
+++ src/Day11.kt
@@ -0,0 +1,110 @@
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.CopyOnWriteArrayList
+import java.util.concurrent.CopyOnWriteArraySet
+
+fun main() {
+    fun parseInput(input: List<String>): AdjacencyList {
+        val adjacencyListGraph = AdjacencyList()
+        
+        input.forEach { line ->
+            val (source, destinations) = line.split(":")
+            destinations.trim().split(" ").forEach { destination ->
+                if (destination.isNotBlank()) {
+                    adjacencyListGraph.addDirectedEdge(source, destination)
+                }
+            }
+        }
+        
+        return adjacencyListGraph
+    }
+    
+    fun part1(input: List<String>): Int {
+        val graph = parseInput(input)
+        
+        return graph.tracePaths("you", "out").filter { it.isNotEmpty() }.size
+    }
+
+    fun part2(input: List<String>): ULong {
+        val graph = parseInput(input)
+
+        return graph.tracePaths("svr", "fft").size.toULong() * 
+                graph.tracePaths("fft", "dac").size.toULong() *
+                graph.tracePaths("dac", "out").size.toULong() +
+                graph.tracePaths("svr", "dac").size.toULong() *
+                graph.tracePaths("dac", "fft").size.toULong() *
+                graph.tracePaths("fft", "out").size.toULong()
+    }
+
+    // Read a large test input from the `src/Day11_test.txt` file:
+    val testInput = readInput("Day11_test")
+    part1(testInput).println()
+    check(part1(testInput) == 5)
+    val testInput2 = readInput("Day11_part2_test")
+    part2(testInput2).println()
+    check(part2(testInput2) == 2.toULong())
+
+    // Read the input from the `src/Day11.txt` file.
+    val input = readInput("Day11")
+    part1(input).println()
+    part2(input).println()
+}
+
+private class AdjacencyList{
+    private val adjacencyMap = mutableMapOf<String, ArrayList<String>>()
+    private val resultList = CopyOnWriteArrayList<List<String>>()
+    private val pathsToDestination = ConcurrentHashMap<String, CopyOnWriteArraySet<List<String>>>()
+
+    fun addDirectedEdge(source: String, destination: String) {
+            if (adjacencyMap[source] == null) {
+                adjacencyMap[source] = arrayListOf(destination)
+            } else {
+                adjacencyMap[source]?.add(destination)
+            }
+    }
+    
+    private fun findPaths(
+        source: String, 
+        destination: String, 
+        currentList: List<String> = emptyList(),
+    ) {
+        if (source == destination) {
+            resultList.add(listOf(source))
+            return
+        }
+        if (adjacencyMap[source]?.isEmpty() == true) return
+        if (currentList.contains(source)) return // Avoid cycles
+        
+        val path = currentList + listOf(source)
+        adjacencyMap[source]?.parallelStream()?.forEach { edge ->
+            if (edge == destination) {
+                with(path + listOf(edge)) {
+                    (0..<size).forEach { 
+                        val partialPath = this.drop(it)
+                        if (partialPath.isNotEmpty()) {
+                            pathsToDestination.getOrDefault(
+                                "${partialPath.first()},$destination", 
+                                CopyOnWriteArrayList()
+                            ).apply {
+                                add(partialPath)
+                            }
+                        }
+                    }
+                    resultList.add(this)
+                }
+                return@forEach
+            } else if (pathsToDestination["$edge,$destination"] != null) {
+                pathsToDestination["$edge,$destination"]?.forEach { resultList.add(currentList + it) }
+                return@forEach
+            }
+            if (adjacencyMap[edge]?.isEmpty() == true) return@forEach
+
+            return@forEach findPaths(edge, destination, path)
+        }
+    }
+    
+    fun tracePaths(source: String, destination: String): List<List<String>> {
+        resultList.clear()
+        findPaths(source, destination)
+        return resultList
+    }
+}
